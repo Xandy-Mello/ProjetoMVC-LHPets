7e9eedba19b173a8591f5a518d45b6caef8c5dcb
Xandy-Mello <122324362+Xandy-Mello@users.noreply.github.com>
2024-01-24 19:07:35 -0300

    primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LHPet.Models;
+
+namespace LHPet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        // Instancias do tipo cliente
+        Cliente cliente1 = new Cliente(01, "Alessandro Mello", "857.032.950-41", "xandymello@sp.senai.br", "Nina");
+        Cliente cliente2 = new Cliente(02, "Adair Zuzo", "039.618.250-09", "adair@gmail.com", "Magrelinho");
+        Cliente cliente3 = new Cliente(03, "Bill Gates", "888.777.920-50", "bill@hotmail.com", "Lobo");
+        Cliente cliente4 = new Cliente(04, "Troval dos Santos", "933.622.400-03", "trovalds@sp.senai.br", "Feio");
+        Cliente cliente5 = new Cliente(05, "Grace Hope", "911.702.988-00", "grace@sp.br", "Robson");
+
+        //lista de clientes
+        List<Cliente> listaClientes = new List<Cliente>();
+        listaClientes.Add(cliente1);
+        listaClientes.Add(cliente2);
+        listaClientes.Add(cliente3);
+        listaClientes.Add(cliente4);
+        listaClientes.Add(cliente5);
+
+        ViewBag.listaClientes = listaClientes;
+
+
+        // Instancias do tipo Fornecedor
+        Fornecedor fornecedor1 = new Fornecedor(01, "C# PET S/A", "14.182.102/0001-80", "c-shark@pet.org");
+        Fornecedor fornecedor2 = new Fornecedor(02, "Ctrl Alt Dog", "15.836.698/0001-57", "ctrl@alt.dog.br");
+        Fornecedor fornecedor3 = new Fornecedor(03, "BootsPet INC", "40.810.224/0001-83", "boots.pet@gatomania.us");
+        Fornecedor fornecedor4 = new Fornecedor(04, "Tik Tok Dogs", "87.945.350/0001-09", "Noisnamidia@tiktokdogs.uk");
+        Fornecedor fornecedor5 = new Fornecedor(05, "Bifinho Forever", "18.760.614/0001-37", "contato@bff.us");
+
+        //Lista de Fornecedores
+        List<Fornecedor> listaFornecedores = new List<Fornecedor>();
+        listaFornecedores.Add(fornecedor1);
+        listaFornecedores.Add(fornecedor2);
+        listaFornecedores.Add(fornecedor3);
+        listaFornecedores.Add(fornecedor4);
+        listaFornecedores.Add(fornecedor5);
+
+        ViewBag.listaFornecedores = listaFornecedores;
+
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
